5a5a816bb14167b1cbb444de3c67ad05f38ccfa1
Cesar <cayocesar.ac@gmail.com>
2020-03-21 03:06:18 -0300

    added youtube fetch data functions

--- src/javascripts/youtube.js
+++ src/javascripts/youtube.js
@@ -1,7 +1,12 @@
 const YoutubeApi = {
     apiKey: "",
     apiUrl: "https://www.googleapis.com/youtube/v3/search",
+    youtubeOembed: "https://www.youtube.com/oembed?url=",
+    
 
+
+    //need apiKey
+    //dont now how to hide the key whithout a backend;
     getApi:async function(){
         const myHeaders = new Headers();
         myHeaders.append('referer', 'https://explorer.apis.google.com');
@@ -12,7 +17,28 @@ const YoutubeApi = {
             method: 'GET',
             headers:myHeaders,
             
-        }).then( (response)=> {return response.json()} );
+        }).then( (response)=> {return response.json()} ).then((data)=>{console.log(data)});
+    },
+
+
+    getVideoById: async function(id){
+        const youtubeVideoUrl = "https%3A//www.youtube.com/watch%3Fv%3D", // + id
+        const url = this.youtubeOembed + this.youtubeVideoUrl + id;
+        console.log(url);
+
+        await fetch(url).then((response)=>{return response.json()});
+    },
+
+
+    //this return a limited amount of searchs
+    videoSearch: async function(search){
+        const url = `https://www.youtube.com/search_ajax?style=json&embeddable=1&search_query=${id}`;
+
+        //console.log(url);
+
+        fetch(url).then(
+            (response)=>{return response.json()}
+        ).then((data)=>{console.log(data)});
     },
 
 
